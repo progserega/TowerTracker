b43d33ab105ca97fbe26098f2eefa89e5eae378a
Семёнов Сергей <semenov@rsprim.ru>
2018-07-11 11:23:24 +1000

    изменения студии

---
+++ app/src/main/java/ru/drsk/progserega/towertracker/selectBySpRes.java
@@ -0,0 +1,14 @@
+package ru.drsk.progserega.towertracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selectBySpRes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_by_sp_res);
+    }
+
+}
